28e4fee19b594adfbf32f7853c5f3b2cf7cb26e9
Rishita <chauhanrishuu@gmail.com>
2022-10-01 16:41:04 +0530

    SWApping of no,

--- add.py
+++ add.py
@@ -1,1 +1,11 @@
-print("Hello")
+x=4
+y=6
+z=x+y
+print("sum is",z)
+
+a=int(input("First no.="))
+b=int(input("Second no. "))
+c=a+b
+print("Sum is",c)
+d=a-b
+print("Subtract is",d)
---
+++ pr.py
@@ -0,0 +1,7 @@
+x=int(input("1 Number="))
+y=int(input("2 Number="))
+z=x
+x=y
+y=z
+print("1 Number =",x)
+print("2 Number=",y)
