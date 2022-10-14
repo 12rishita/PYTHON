37614d1ef275c355a32b40b7a44ec7635ce52691
Rishita <chauhanrishuu@gmail.com>
2022-10-14 19:32:14 +0530

    PRACTICE QUESTION

--- Attendence.py
+++ Attendence.py
@@ -1,11 +1,11 @@
 x=int(input("Please enter your Attendence percentage="))
-if x>=95:
+if x>=95 and x<100:
     print("You will ge 5 MARKS")
-elif x>=90:
+elif x>=90 and x<100:
     print("You will get 4 MARKS")
-elif x>=85:
+elif x>=85 and x<100:
     print("You will get 3 MARKS")
-elif x>=75:
+elif x>=75 and x<100:
     print("You will get 2 MARKS")
 else:
     print("You will get 0 MARKS")
