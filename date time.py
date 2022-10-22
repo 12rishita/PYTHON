2635c18785b0c0aa2b9172a08d1dc5350aed814f
Rishita <chauhanrishuu@gmail.com>
2022-10-22 17:59:27 +0530

    practice

---
+++ date time.py
@@ -0,0 +1,13 @@
+# importing date class from datetime module
+from datetime import date
+
+# creating the date object of today's date
+date = date.today()
+
+# printing todays date
+print("Current date: ", date)
+
+# fetching the current year, month and day of today
+print("Current year:", date.year)
+print("Current month:", date.month)
+print("Current day:",date.day)
---
+++ practice/22.py
@@ -0,0 +1,6 @@
+a=int(input(""))
+b=int(input(""))
+if a<0 and b>0 or b>0 and a>0:
+    print("True")
+else:
+    print("False")
