bd22202207f410753435f26bee3ba197afdead10
Rishita <chauhanrishuu@gmail.com>
2022-10-29 13:57:54 +0530

    practice

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+#while loop  or a+=1 for break"if a==
+a=2
+while a<200:
+    print(a)
+    if a==20:
+        continue
+    if a==156:
+        break
+    a=a+22
+# ques
+b=1
+while b<=4:
+    print("*"*b)
+    b=1+b
+#write a program to print first 10 even numbers using while loop
+num=2
+while num<21:
+    print(num)
+    num=num+2 
