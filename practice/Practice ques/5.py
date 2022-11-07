01ca22294bc2332eab70db8a0eb4850c1aa38d99
Rishita <chauhanrishuu@gmail.com>
2022-11-07 08:55:06 +0530

    Homework

---
+++ practice/Practice ques/5.py
@@ -0,0 +1,5 @@
+list1= ["Phil","Oz","Seuss","dre"]
+n=len(list1)
+for i in list1:
+    list1.append("Dr.")
+print(i)
---
+++ practice/Practice ques/6.py
@@ -0,0 +1,6 @@
+#factorial
+num=int(input("Enter any number="))
+n=1
+for i in range(1,num+1):
+    n=n*i
+print(n)
