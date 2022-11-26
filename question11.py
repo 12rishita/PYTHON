989b42b2f12baf83e6d4897e8f2f070cba22198c
Rishita <chauhanrishuu@gmail.com>
2022-11-26 16:11:18 +0530

    practice

--- question11.py
+++ question11.py
@@ -1,3 +1,4 @@
+#question1
 tuple1 = (10,20)
 tuple2 = (30,40)
 #Swap above tuples
@@ -5,4 +6,18 @@ tuple3=tuple1
 tuple1=tuple2
 tuple2=tuple3
 print(tuple1)
-print(tuple2)
+print(tuple2 )
+
+#question2 
+mytuple=("car","boat","bike")
+#for i in mytuple:
+#    print(i)
+i=0
+# while i<len(mytuple):
+#     print(mytuple[i])
+#     i=i+1
+######CREATING LIST IN TUPLE
+t=(1,[6,7],2,3,4,5)
+t6=[1]
+t[1][0]=8
+print(t)
